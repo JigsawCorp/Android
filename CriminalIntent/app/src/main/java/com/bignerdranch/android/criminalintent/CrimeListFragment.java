9ad439886eb40f3a81a0de3de4b8a1d0a2d61167
Baboo <philbond1997@hotmail.com>
2018-05-28 09:02:22 -0400

    Chapter 14 challenge completed

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -79,6 +79,7 @@ public class CrimeListFragment extends Fragment {
             mCrimeRecyclerView.setAdapter(mAdapter);
         }
         else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
 
@@ -140,6 +141,10 @@ public class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
         }
 
     }
@@ -210,4 +215,6 @@ public class CrimeListFragment extends Fragment {
             mCreateCrimeButton.setVisibility(View.GONE);
         }
     }
+
+
 }
