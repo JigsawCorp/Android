856537c2818a7d1e6be991d53879f74872983ae5
Baboo <philbond1997@hotmail.com>
2018-06-07 12:34:06 -0400

    Chapter 21 finished

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -1,8 +1,11 @@
 package com.bignerdranch.android.beatbox;
 
+import android.content.Intent;
+
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
@@ -13,6 +16,14 @@ public class Sound {
 
     public String getAssetPath() {
         return mAssetPath;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 
     public String getName() {
