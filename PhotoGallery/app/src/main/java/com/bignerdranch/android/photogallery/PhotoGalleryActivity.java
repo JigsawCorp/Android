c088f5765157cc24f2c65744081f2cb4e96a5654
Jigsaw <philbond1997@hotmail.com>
2018-06-15 12:25:25 -0400

    Chapter 28 finished

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.photogallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +9,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {
